b05af6b94976823f231b6ab0cf618f638ee62da2
Juanes1805 <168312925+Juanes1805@users.noreply.github.com>
2024-09-16 13:35:02 -0500

    Update calendar.py

--- app/model/calendar.py
+++ app/model/calendar.py
@@ -6,7 +6,6 @@ from app.services.util import generate_unique_id, date_lower_than_today_error, e
     reminder_not_found_error, slot_not_available_error
 
 
-# TODO: Implement Reminder class here
 @dataclass
 class Reminder:
     date_time: datetime
@@ -18,7 +17,6 @@ class Reminder:
         f"Reminder on {self.date_time} of type {self.type}"
 
 
-# TODO: Implement Event class here
 @dataclass
 class Event:
     title: str
@@ -26,10 +24,10 @@ class Event:
     date_: date
     start_at: time
     end_at: time
-    reminders: list[Reminder] = field(default_factory=list)
+    reminders: list[Reminder] = field(init=False, default_factory=list)
     id: str = field(default_factory=generate_unique_id)
 
-    def add_reminder(self, date_time: datetime, type_: str = Reminder.EMAIL):
+    def add_reminder(self, date_time: datetime, type_: str):
         self.reminders.append(Reminder(date_time, type_))
 
     def delete_reminder(self, reminder_index: int):
@@ -43,7 +41,6 @@ class Event:
                 f"Event title: {self.title}\n"
                 f"Description: {self.description}\n"
                 f"Time: {self.start_at} - {self.end_at}")
-# TODO: Implement Day class here
 
 
 class Day:
@@ -51,18 +48,19 @@ class Day:
     def __init__(self, date_: date):
         self.date_: date = date_
         self.slots: dict[time, str | None] = {}
-
+        self._init_slots()
+        
     def _init_slots(self):
         for h in range(24):
-            for m in [0, 15, 30, 45]:
+            for m in range(0, 60, 15):
                 self.slots[time(h, m)] = None
 
     def add_event(self, event_id: str, start_at: time, end_at: time):
         for slot in self.slots:
             if start_at <= slot < end_at:
                 if self.slots[slot] is not None:
-                    from app.services.util import slot_not_available_error
                     slot_not_available_error()
+                else:
                 self.slots[slot] = event_id
 
     def delete_event(self, event_id: str):
@@ -96,5 +94,33 @@ class Calendar:
     def add_event(self, title: str, description:str, date_: date, start_at: time, end_at: time):
         if date_ < datetime.now().date():
             date_lower_than_today_error()
+            
         if date_ not in self.days:
             self.days[date_] = Day(date_)
+
+        event = Event(title, description, date_, start_at, end_at)
+        self.days[date_].add_event(event.id, start_at, end_at)
+        self.events[event.id] = event
+
+        return event.id
+
+    def add_reminder(self, event_id: str, date_time: datetime, type_: str):
+        event = self.events.get(event_id)
+        if not event:
+            event_not_found_error()
+
+        event.add_reminder(date_time, type_)
+
+    def find_available_slots(self, date_: date) -> list[time]:
+        available_slots = []
+        day = self.days.get(date_)
+        if day:
+            for slot, event in day.slots.items():
+                if not event:
+                    available_slots.append(slot)
+        else:
+            day: Day(date_)
+            available_slots = list(day.slots.keys())
+
+        return available_slots
+                                
